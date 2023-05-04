bc674590df5a143f7c486b99750d20622d76b182
Jelle Kraaijeveld <jelle.kraaijeveld@student.hku.nl>
2023-05-04 15:03:15 +0200

    added: thuis.jp

--- image_analysis/nostalgia_cpp/main.cpp
+++ image_analysis/nostalgia_cpp/main.cpp
@@ -2,7 +2,7 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
-    cv::Mat srcImage = cv::imread("/Users/jellekraaijeveld/Documents/HKU1/Jaar_2/CSD/nostalgie/image_analysis/cpp_nostalgie/thuis.jpg"); // Use full path!
+    cv::Mat srcImage = cv::imread("/Users/jellekraaijeveld/Documents/HKU1/Jaar_2/CSD/nostalgie/image_analysis/nostalgia_cpp/thuis.jpg"); // Use full path!
     cv::imshow("[img]", srcImage);
     cv::waitKey(0);
     return 0;
